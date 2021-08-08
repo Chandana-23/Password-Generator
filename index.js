b4b6f810bd259dbb579366816503dac517586e6e
Code Sandbox <hello@codesandbox.io>
2021-08-08 06:01:45 +0000

    Deployment from Codesandbox.

---
+++ index.js
@@ -0,0 +1,10 @@
+function fun() {
+  var pass = "";
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+  for (let i = 1; i <= 8; i++) {
+    var char = Math.floor(Math.random() * str.length + 1);
+    pass += str.charAt(char);
+  }
+
+  document.getElementById("answer").innerHTML = "Password is " + pass;
+}
